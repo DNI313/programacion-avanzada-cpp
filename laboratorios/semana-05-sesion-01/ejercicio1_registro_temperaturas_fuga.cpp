d04d4a57c8ceea1fbdd4251880c343fba1e0e2f7
DNI313 <dr602730@gmail.com>
2026-09-14 13:00:42 +0000

    Semana 5 clase 1

--- laboratorios/semana-05-sesion-01/ejercicio1_registro_temperaturas_fuga.cpp
+++ laboratorios/semana-05-sesion-01/ejercicio1_registro_temperaturas_fuga.cpp
@@ -19,8 +19,10 @@ class RegistroTemperaturas {
             return lecturas[indice];
         }
 
-        // TODO: falta el destructor. Sin el, la memoria de "lecturas"
-        // nunca se libera cuando el objeto deja de existir.
+        ~RegistroTemperaturas() {
+            delete[] lecturas;
+            std::cout << "Memoria liberada" << std::endl;
+        }
 };
 
 void registrarDia(int capacidad) {
@@ -33,6 +35,7 @@ void registrarDia(int capacidad) {
 int main() {
     // TODO: antes de correr, predice: ¿cuantas veces esperas ver
     // "Memoria liberada" en la salida, si registramos 3 dias seguidos?
+
     registrarDia(5);
     registrarDia(5);
     registrarDia(5);
